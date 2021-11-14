4779b99bd299a28c28b78b9d1b2cf0af4754a494
Lawrence Lee <63452267+renceLaw515@users.noreply.github.com>
2021-11-14 12:31:33 +0800

    Update pipe_size.py

--- pipe_size.py
+++ pipe_size.py
@@ -3,12 +3,10 @@ import argparse
 
 class WaterPipeManager:
     
-    def __init__(self, pipe_list, flow_list, low_bound_velocity, upper_bound_velocity, tol):
+    def __init__(self, pipe_list, flow_list, tol):
 
         self.pipe_list = pipe_list 
         self.flow_list = flow_list
-        self.low_bound_velocity = low_bound_velocity
-        self.upper_bound_velocity = upper_bound_velocity
         self.tol = tol
 
     def compute_velocity(self, q, d):
@@ -34,15 +32,12 @@ class WaterPipeManager:
         return result
 
 if __name__ == "__main__":
-    ## ===== ===== ===== ===== ===== ===== ===== =====
+## ===== ===== ===== ===== ===== ===== ===== =====
 ## Parse arguments
 ## ===== ===== ===== ===== ===== ===== ===== =====
 
     parser = argparse.ArgumentParser(description = "Water Pipe Sizer");
 
-
-    parser.add_argument('--low_b', type=float, default=1.5, help='Lower Bound Velocity Requiremnt of Water Pipe');
-    parser.add_argument('--up_b', type=float, default=1.5, help='Higher Bound Velocity Requiremnt of Water Pipe');
     parser.add_argument('--pipe_dia', action='store',
                     type=int, nargs='*', default=[25,32,40,50,65,80,100,150,200,250,300],
                     help="Pipe Diameters for consideration")
@@ -53,7 +48,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args();
 
-    worker = WaterPipeManager(args.pipe_dia, args.flow_list, args.low_b, args.up_b, args.tol)
+    worker = WaterPipeManager(args.pipe_dia, args.flow_list, args.tol)
     print(worker.size_water_pipe())
 
 
